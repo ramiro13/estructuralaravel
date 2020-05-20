92f38926a6bd713e7a54ee19d6daae8053f6adf5
ramiro13 <ramiroperezsanz13@gmail.com>
2020-05-20 23:22:52 +0200

    laravel7

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class NotasController extends Controller
 {
     public function index(){
-        return view('notas.todas.index',['notas' => Notas::all()]);
+        return view('notas.todas.index',['notas' => Notas::all()->where('user_id',auth()->id())]);
     }
 
     public function store(){
@@ -29,5 +29,18 @@ class NotasController extends Controller
 
     public function archivadas(){
         return view('notas.archivadas');
+    }
+
+    public function edit($id){
+        return view('notas.todas.edit',['nota'=>Notas::findOrFail($id)]);
+    }
+
+    public function update (Request $request,$id){
+        $nota=Notas::findORFail($id);
+
+        $nota->titulo = request('titulo');
+        $nota->texto = request('texto');
+
+        $nota->update();
     }
 }
--- resources/views/notas/todas/index.blade.php
+++ resources/views/notas/todas/index.blade.php
@@ -11,12 +11,23 @@
 @foreach ($notas as $nota)
 
 <div class="col-4">
-    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+    <div class="card border-primary mb-3" style="max-width: 18rem;">
         <div class="card-header p-3"> <b>{{ $nota->titulo }}</b>  <div class="float-right">{{ $nota->created_at }}</div> </div>
         <div class="card-body">
             <p class="card-text">{{ $nota->texto }}</p>
         </div>
+        <div class="card-footer">
+
+        <button type="button" class="btn btn-danger float-right ml-2"><i class="far fa-trash-alt"></i></button>
+
+        <a href="{{URL::action('NotasController@edit',$nota->id)}}">
+        <button type="button" class="btn btn-info float-right ">
+            <i class="far fa-edit"></i>
+        </button>
+        </a>
     </div>
+    </div>
+
 </div>
 
 @endforeach
