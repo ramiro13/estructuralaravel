0dc4ffbb3b1d132e53a069b4d73ca50ecc419506
ramiro13 <ramiroperezsanz13@gmail.com>
2020-05-22 16:07:32 +0200

    roles admin por usuarios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -198,19 +198,19 @@
                                     <p>Inicio</p>
                                 </a>
                             </li>
-
+                            @can('administrador')
                             <li class="nav-item">
                                 <a href="{{ url('usuarios') }}"
                                     class="{{ Request::path() === 'usuarios' ? 'nav-link active' : 'nav-link' }}">
                                     <i class="nav-icon fas fa-users"></i>
                                     <p>
                                         Usuarios
-                                        <?php use App\User; $users_count = User::all()->count(); ?>
+                                        <?php $users_count = DB::table('users')->count(); ?>
                                         <span class="right badge badge-danger">{{ $users_count ?? '0' }}</span>
                                     </p>
                                 </a>
                             </li>
-
+                            @endcan
                             <li class="nav-item has-treeview">
                                 <a href="#" class="nav-link">
                                     <i class="nav-icon far fa-sticky-note"></i>
